0fb0a78455a0809792d4fb58cf5bb6590cdb0a2d
Paul Smith <paul@onlyforart.com>
2025-12-11 09:47:33 +0000

    redirect from workers.dev to onlyforlove.eu

--- website/src/main.js
+++ website/src/main.js
@@ -2,4 +2,17 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Redirect from workers.dev to primary domain if reachable
+if (window.location.hostname === '5rworkshops.onlyforart.workers.dev') {
+  const primaryUrl = 'https://5rworkshops.onlyforlove.eu'
+  fetch(primaryUrl, { method: 'HEAD', mode: 'no-cors' })
+    .then(() => {
+      window.location.href = primaryUrl
+    })
+    .catch(() => {
+      // Primary domain not reachable, stay on current site
+      createApp(App).mount('#app')
+    })
+} else {
+  createApp(App).mount('#app')
+}

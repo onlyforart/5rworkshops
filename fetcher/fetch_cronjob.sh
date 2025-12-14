b5b95d45c986e6b1226b60a489962dd99b874e4f
Paul Smith <paul@onlyforart.com>
2025-12-14 15:09:17 +0000

    updated event list (Sun 14 Dec 2025 15:07:53 GMT)

--- fetcher/fetch_cronjob.sh
+++ fetcher/fetch_cronjob.sh
@@ -3,10 +3,12 @@
 # Change to the script's directory
 cd "$(dirname "$0")"
 
+NEWDATE=$( date )
+
 # Execute fetch.js and only proceed if successful
-if node fetch-events.js; then
+if "./fetch.sh"; then
     cd ..
     git add -A
-    git commit -m "updated event list"
+    git commit -m "updated event list ($NEWDATE)"
     git push origin main
 fi
